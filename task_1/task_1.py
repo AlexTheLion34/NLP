398a691c9b04890b948c1b1812784d8263646839
AlexTheLion34 <alx_pet@mail.ru>
2021-03-19 14:39:02 +0300

    Final changes

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -1,6 +1,8 @@
 import re
 from nltk.stem.snowball import SnowballStemmer
 import pymorphy2
+import sys
+import csv
 
 
 def tokenize(source, regexp):
@@ -10,60 +12,109 @@ def tokenize(source, regexp):
         return regexp.findall(source)
 
 
-# Task 1
+# Задание 1
 word = r'[A-ZА-Яa-zа-яёË]+'
-word_and_separator = r'([A-ZА-Яa-zа-яёË]+|[.,!?;:]+)'
-phone_number = r'((\+7|8)[\-( ]?(\d{3})[\-) ]?(\d{3})[\- ]?(\d{2})[\- ]?(\d{2}))'
-emoticons = r'(:[\(\)])+'
+word_and_separator = r'[A-ZА-Яa-zа-яёË]+|[.,!?;:]+'
+phone_number = r'(\+7|8)[\-( ]?(\d{3})[\-) ]?(\d{3})[\- ]?(\d{2})[\- ]?(\d{2})'
+emoticons = r'[\:][\)\(]'
 
 generic = re.compile('(%s|%s|%s)' % (word_and_separator, phone_number, emoticons))
 
-main_text = 'Этот текст предназначен для теста регулярных выражений. ' \
-            'Same, but written in English: This text is for testing ' \
-            'regular expressions. Вот несколько мобильных телефонов, ' \
-            'для тестирования: +79114567890, 89114567890, ' \
-            '+7-911–456-78-90, 8 911 456 78 90, 8(911)456 78 90, ' \
-            '+7(911)4567890. Должно работать. Must work. Точно работает?' \
-            'Вот тут проверяются еще нектороые знаки препинания ; !!!' \
-            'Теперь проверим смайлы :). Несколько смайлов :):). И Грустный тоже (:.'
+# Чехов
+main_text = '''
+    Жизнь пренеприятная штука, но сделать ее прекрасной очень нетрудно. Для того, чтобы ощущать 
+    в себе счастье без перерыва, 
+    даже в минуты скорби и печали, нужно: уметь довольствоваться настоящим и радоваться сознанию, что “могло бы быть 
+    и хуже”. А это нетрудно:
+    Когда у тебя в кармане загораются спички, то радуйся и благодари небо, что у тебя в кармане не пороховой погреб.
+    Когда в твой палец попадает заноза, радуйся: “Хорошо, что не в глаз”!
+    Если твоя жена… играет гаммы, то не выходи из себя, а не находи себе место от радости, что ты слушаешь игру, 
+    а не вой шакалов…
+    Радуйся, что ты не лошадь, не свинья, не осел, не медведь, которого водят цыгане, не клоп… Радуйся, что ты не 
+    хромой, не слепой, не глухой, не немой, не холерный…
+    Если у тебя болит один зуб, то ликуй, что у тебя болят не все зубы.
+    Если жена тебе изменила, то радуйся, что она изменила тебе, а не отечеству.
+    И так далее…
+    Вот несколько мобильных телефонов, для тестирования: +79114567890, 89114567890, 
+    +7-911–456-78-90, 8(911)456 78 90, +7(911)4567890. Грусть:(. Радость:):).
+'''
 
-file_1 = open('./res/task_1.txt', 'a')
+file_1 = open('res/task_1.txt', 'a')
 
 for token in tokenize(source=main_text, regexp=re.compile(pattern=generic)):
     file_1.write(str(token) + '\n')
 
 file_1.close()
 
-# Task 2
+# Задание 2
 stemmer = SnowballStemmer("russian")
 
-file_2 = open('./res/task_2.txt', 'a')
+file_2 = open('res/task_2.txt', 'a')
 
-for token in tokenize(source=main_text, regexp=re.compile(pattern=word)):
+# Тескт, разбитый на слова
+tokens = tokenize(source=main_text, regexp=re.compile(pattern=word))
+
+for token in tokens:
     file_2.write(str(token) + ' ' + str(stemmer.stem(token)) + '\n')
 
 file_2.close()
 
-# Task 3
-morph = pymorphy2.MorphAnalyzer()
+# Задание 3
+morph = pymorphy2.MorphAnalyzer(lang='ru')
 
-morph_text = 'С камушка на камушек порхал воробышек, а на террасе, искусно задрапированной гобеленами с дефензивой ' \
-             'кронштадтского инфантерийского батальона, под искусственным абажуром, закамуфлированным под ' \
-             'марокканский минарет, веснушчатая свояченица вдовствующего протоиерея Агриппина Саввична потчевала ' \
-             'коллежского асессора, околоточного надзирателя и индифферентного ловеласа Фаддея Аполлинарьевича ' \
-             'винегретом со снетками.'
+dataset = open('../../../dict.tsv')
 
-file_3 = open('./res/task_3.txt', 'a')
+read_tsv = csv.reader(dataset, delimiter="\t")
+csv.field_size_limit(sys.maxsize)
 
-for token in tokenize(source=morph_text, regexp=re.compile(pattern=word)):
-    file_3.write(str(token) + ' ' + str(morph.parse(token)) + '\n')
+# Мапа токенов и лемм из словаря
+map_of_dict_lemmas = dict()
 
-file_3.close()
+for row in read_tsv:
+    map_of_dict_lemmas[row[0]] = (row[1], row[2])
 
-# Task 4
-file_4 = open('./res/Petrenko.tsv', 'a')
+dataset.close()
 
-for token in tokenize(source=main_text, regexp=re.compile(pattern=word)):
+# Мапа токенов и лемм из текста
+map_of_my_lemmas = dict()
+
+for token in tokens:
+    map_of_my_lemmas[str(token)] = str(morph.parse(token)[0].normal_form), str(morph.parse(token)[0].tag)
+
+file_3_1 = open('res/task_3_1.txt', 'a')
+file_3_1.write('Dict, My\n')
+
+# Сопоставление токенов из словаря и текста
+for key in map_of_my_lemmas.keys():
+    if key in map_of_dict_lemmas.keys():
+        file_3_1.write(str(key)
+                       + ' : '
+                       + str(map_of_dict_lemmas[key])
+                       + ', '
+                       + str(key)
+                       + ' : '
+                       + str(map_of_my_lemmas[key]) + '\n')
+# 10 строчка
+# 77 строчка
+file_3_1.close()
+
+file_3_2 = open('res/task_3_2.txt', 'a')
+
+# Выявление токенов, для которых было найдено более 1 леммы
+for token in tokens:
+    res = morph.parse(token)
+    if str(token) in map_of_dict_lemmas.keys() and len(res) > 1:
+        lemmas = []
+        for parse in res:
+            lemmas.append((parse.normal_form, parse.tag))
+            file_3_2.write(str(token) + ' : ' + str(lemmas) + '\n')
+
+file_3_2.close()
+
+# Задание 4
+file_4 = open('res/Petrenko.tsv', 'a')
+
+for token in tokens:
     file_4 \
         .write(str(token) + '\t' + str(stemmer.stem(token)) + '\t' + str(morph.parse(token)[0].normal_form) + '\n')
 
